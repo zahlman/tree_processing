cadc4db75b908b454abf395533b13ebc2d0f500b
Karl Knechtel <zahlman@proton.me>
2025-10-16 18:30:19 -0400

    Test with slightly more sophisticated logic

--- test/test_filesystem.py
+++ test/test_filesystem.py
@@ -1,16 +1,18 @@
 from pathlib import Path
 
-from tree_processing.filesystem import fake_propagate_folders, fake_copy_files, get_children, not_hidden
+from tree_processing.filesystem import fake_propagate_folders, fake_copy_regular_files, get_children, not_hidden
 from tree_processing.traversal import topdown
 from tree_processing.actions import _rejected, Node
+
 
 def test_fake_copy():
     root = Node(True, None, (Path('.'), Path('/tmp')), None)
     recurse_branches = []
     traversal = topdown(root, get_children)
     node = next(traversal)
-    process_folder = fake_propagate_folders.which(not_hidden)
-    process_file = fake_copy_files.which(not_hidden)
+    not_pycache = lambda node: node.name != '__pycache__'
+    process_folder = fake_propagate_folders.which(not_hidden & not_pycache)
+    process_file = fake_copy_regular_files.which(not_hidden)
     while True:
         try:
             if node.internal:
