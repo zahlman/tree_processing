33c30cd9447029530705d372670c21408b015a89
Karl Knechtel <zahlman@proton.me>
2025-10-16 18:03:41 -0400

    Simplify `hardlink_or_copy`
    Per the documentation:
    
    > If not separately noted, all functions that claim "Availability: Unix"
    > are supported on macOS, iOS and Android, all of which build on a Unix
    > core.
    
    Thus, it is now assumed that `os.link` will be available on all
    platforms (and is not excluded on Mac as previously imagined).

--- src/tree_processing/__init__.py
+++ src/tree_processing/__init__.py
@@ -1,5 +1,5 @@
 from functools import partial
-from os import PathLike, walk
+from os import PathLike, link, walk
 from pathlib import Path
 from shutil import copy
 from typing import Any, Callable, NamedTuple, NewType, Optional
@@ -8,16 +8,11 @@ from typing import Any, Callable, NamedTuple, NewType, Optional
 # Path.hardlink_to requires 3.10.
 # For better compatibility, we fall back to the os module
 # and also fall back on copying if hardlinks fail for any reason.
-try:
-    from os import link as _hardlink
-except ImportError: # on MacOS, copy instead.
-    hardlink_or_copy = copy
-else:
-    def hardlink_or_copy(src, dst):
-        try:
-            _hardlink(src, dst)
-        except:
-            copy(src, dst)
+def hardlink_or_copy(src, dst):
+    try:
+        link(src, dst)
+    except:
+        copy(src, dst)
 
 
 OnError = NewType('OnError', Optional[Callable[[OSError], Any]])
