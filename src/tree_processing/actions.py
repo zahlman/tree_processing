4c20270674c690f8a78df8bb4d9ba88d03f0981f
Karl Knechtel <zahlman@proton.me>
2025-10-16 17:45:38 -0400

    Remove misleading comment
    The proposed signaling mechanism is impossible with namedtuples (and
    dataclasses would cost support for Python 3.6 or require extra work).

--- src/tree_processing/actions.py
+++ src/tree_processing/actions.py
@@ -6,8 +6,6 @@ from typing import Any, Callable, NamedTuple, NewType, Self
 class Node(NamedTuple):
     '''Structure representing a node in a traversal.'''
     internal: bool # whether the node may potentially have children.
-    # In top-down traversals, this value is checked after processing,
-    # so it can be set to False to prevent recursing over children.
     name: Any # a relationship between the parent and current nodes.
     current: Any # The current node in the traversal.
     parent: Any # The parent of the current node in the traversal.
