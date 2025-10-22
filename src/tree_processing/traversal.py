f6cdd99f63825a3dd9c23a815df42598b6b6613b
Karl Knechtel <zahlman@proton.me>
2025-10-22 15:40:13 -0400

    Refactoring

--- src/tree_processing/traversal.py
+++ src/tree_processing/traversal.py
@@ -1,17 +1,19 @@
+def _reorder(children, stack):
+    internal = []
+    for c in children:
+        if c.internal:
+            internal.append(c) # for second pass
+        else:
+            yield c
+    for c in internal:
+        if (yield c):
+            stack.append(c)
+
+
 def topdown(root, get_children):
     stack = [root]
     while stack:
         top = stack.pop()
         if not top.internal:
             continue # skipped by processing
-        children = get_children(top)
-        internal = []
-        for c in children:
-            if not c.internal:
-                yield c
-            else:
-                internal.append(c) # for second pass
-        for c in internal:
-            recurse = (yield c)
-            if recurse:
-                stack.append(c)
+        yield from _reorder(get_children(top), stack)
