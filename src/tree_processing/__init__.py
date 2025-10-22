933cd50a3195c0d29ae8891185ec1379b7f15be5
Karl Knechtel <zahlman@proton.me>
2025-10-22 13:50:27 -0400

    Implement main algorithm
    The code from the test suite manually using the lower-level pieces has
    been adapted into a main `process` function. This additionally accepts
    an `accumulator` and `total`, which allow for combining return values
    (other than `_rejected`) from processing individual nodes.

---
+++ src/tree_processing/__init__.py
@@ -0,0 +1,30 @@
+from tree_processing.actions import _rejected
+
+
+def _process_one(node, traversal, act, accumulate, total):
+    result = act[0 if node.internal else 1](node)
+    accepted = (result is not _rejected)
+    next_node = traversal.send(accepted)
+    if accepted and accumulate is not None:
+        total = accumulate(total, result)
+    return next_node, total
+
+
+def _process(traversal, act, accumulate, total):
+    node = next(traversal)
+    while True:
+        try:
+            node, total = _process_one(node, traversal, act, accumulate, total)
+        except StopIteration:
+            return total
+
+
+def process(traverse, root, get_children, act, accumulate=None, total=None):
+    traversal = traverse(root, get_children)
+    try:
+        process_folder, process_file = act
+    except TypeError: # not iterable
+        # If an iterable is provided but it has the wrong number of
+        # callables, that `ValueError` should propagate.
+        act = act, act
+    return _process(traversal, act, accumulate, total)
--- test/test_filesystem.py
+++ test/test_filesystem.py
@@ -3,24 +3,14 @@ from pathlib import Path
 from tree_processing.actions.filesystem import fake_propagate_folders, fake_copy_regular_files, not_hidden
 from tree_processing.generators.filesystem import get_children
 from tree_processing.traversal import topdown
-from tree_processing.actions import _rejected, Node
+from tree_processing.actions import Node
+from tree_processing import process
 
 
 def test_fake_copy():
     root = Node(True, None, (Path('.'), Path('/tmp')), None)
-    recurse_branches = []
-    traversal = topdown(root, get_children)
-    node = next(traversal)
     not_pycache = lambda node: node.name != '__pycache__'
     process_folder = fake_propagate_folders.which(not_hidden & not_pycache)
     process_file = fake_copy_regular_files.which(not_hidden)
-    while True:
-        try:
-            if node.internal:
-                result = process_folder(node) is not _rejected
-            else:
-                result = process_file(node)
-            node = traversal.send(result)
-        except StopIteration:
-            break
-    assert False # for now, to see output
+    # This will fail, but lets us see output for now.
+    assert process(topdown, root, get_children, (process_folder, process_file))
