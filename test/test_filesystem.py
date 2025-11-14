304c8d8845080ad7237c56390e87332088ea3aeb
Karl Knechtel <zahlman@proton.me>
2025-11-14 16:02:31 -0500

    Fix test setup
    * Expected results for tests are now recorded in TOML files.
    
    * The test fixture now properly reads the TOML files and reports basic
      information from them.
    
    * Tests are configured to use the "expected" data from the TOML.
      Additionally, the accumulator tests now correctly use the test-case
      folder structures rather than the working tree.
    
    * Tests fully pass, for now at least.

--- test/test_filesystem.py
+++ test/test_filesystem.py
@@ -6,6 +6,10 @@ from tree_processing import process, accumulator
 
 from os import chdir, getcwd, mkdir
 from pathlib import Path
+try:
+    from tomllib import load as load_toml
+except ImportError:
+    from tomli import load as load_toml
 from zipfile import ZipFile
 
 from pytest import fixture, mark
@@ -16,31 +20,32 @@ TREE_DIR = Path(__file__).parent / 'trees'
 
 
 @fixture(params=('empty', 'sample'))
-def setup(tmpdir, request):
+def expected(tmpdir, request):
     old = getcwd()
     # Unzip a sample file hierarchy directly into the tmpdir.
     name = request.param
     with ZipFile(TREE_DIR / f'{name}.zip') as zf:
         zf.extractall(tmpdir)
     chdir(tmpdir)
-    yield (TREE_DIR / f'{name}.toml').read_text()
+    with open(TREE_DIR / f'{name}.toml', 'rb') as f:
+        yield load_toml(f)['expected']
     chdir(old)
 
 
-def test_fake_copy(tmpdir, setup):
+def test_fake_copy(expected, capsys):
     root = make_root('.', '/tmp')
     process_folder = fake_propagate_folders.which(not_hidden)
     process_file = fake_copy_regular_files.which(not_hidden)
     topdown(root, default_get)(process_folder, process_file)
-    assert False # just see the printed output for now.
+    assert capsys.readouterr().out == expected['fake_copy']
 
 
-def test_naive_iterate(tmpdir, setup):
+def test_naive_iterate(expected, capsys):
     root = make_root('.', '/tmp')
     for node in topdown(root, default_get):
         src, dst = node.current
         print(f"mirror {src} -> {dst}")
-    assert False
+    assert capsys.readouterr().out == expected['naive_iterate']
 
 
 def folder_count(f):
@@ -55,10 +60,11 @@ def pairwise_sum(t1, t2):
     return (t1[0] + t2[0], t1[1] + t2[1])
 
 
-def test_count_immutable():
-    root = make_root('src')
+def test_count_immutable(expected):
     act = accumulator((0, 0), pairwise_sum)(folder_count, file_count)
-    assert not topdown(make_root('src'), default_get)(act)
+    result = topdown(make_root('.'), default_get)(act)
+    assert isinstance(result, tuple)
+    assert result == tuple(expected['count'])
 
 
 def pairwise_add(t1, t2):
@@ -67,7 +73,8 @@ def pairwise_add(t1, t2):
     return t1
 
 
-def test_count_mutable():
-    root = make_root('src')
+def test_count_mutable(expected):
     act = accumulator([0, 0], pairwise_add)(folder_count, file_count)
-    assert not topdown(make_root('src'), default_get)(act)
+    result = topdown(make_root('.'), default_get)(act)
+    assert isinstance(result, list)
+    assert result == expected['count']
