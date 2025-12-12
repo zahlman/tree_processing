881cfed701881e96fcc688c853e8e01cce7a7537
Karl Knechtel <zahlman@proton.me>
2025-12-11 23:38:19 -0500

    Fix iteration order
    In order for sorted folder and file names to be processed in order,
    while still performing a depth-first traversal, it's necessary to
    reverse folder names before putting them on the stack.
    
    This is awkward and it would be better to reverse the names for internal
    nodes in the sorting routine (after first figuring out a cleaner way to
    offer sorting), so as not to incur the overhead (especially since *most*
    traversals will likely *not* expect sorted output).

--- src/tree_processing/traversal.py
+++ src/tree_processing/traversal.py
@@ -3,11 +3,17 @@ from . import rejected, traversal
 
 
 def _recurse_over(children, stack):
+    # The first internal node should go on the stack last, so that it will be
+    # processed first. But children should be iterated in order, because
+    # they're being yielded directly.
+    # (We must use a stack in order to have a depth-first traversal.)
+    to_push = []
     for c in children:
         if c.internal:
-            stack.append(c)
+            to_push.append(c)
         else:
             yield c
+    stack.extend(to_push[::-1])
 
 
 @traversal
